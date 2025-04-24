023b346e7e6a8c2cfb4a7b81fe085ae3fceec9a1
kerosene2562 <ipzk241_roa@student.ztu.edu.ua>
2025-04-24 23:31:03 +0300

    lab4_task4

---
+++ Composite/FileImageLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3.Composite
+{
+    public class FileImageLoader : IImageLoaderStrategy
+    {
+        public string Load(string href)
+        {
+            return $"Image loaded from file: {Path.GetFileName(href)}";
+        }
+    }
+}
---
+++ Composite/LightImageNode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3.Composite
+{
+    class LightImageNode : LightNode
+    {
+        public string Src { get; set; }
+        private IImageLoaderStrategy _loaderStrategy;
+
+        public LightImageNode(string href)
+        {
+            Src = href;
+            SetStrategy(href);
+        }
+
+        private void SetStrategy(string href)
+        {
+            if (href.StartsWith("http://") || href.StartsWith("https://"))
+                _loaderStrategy = new NetworkImageLoader();
+            else
+                _loaderStrategy = new FileImageLoader();
+        }
+
+        public override string OuterHTML()
+        {
+            return $"<img src=\"{Src}\" alt=\"{_loaderStrategy.Load(Src)}\" />";
+        }
+
+        public override string InnerHTML()
+        {
+            return string.Empty;
+        }
+    }
+}
