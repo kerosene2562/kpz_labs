3fe59b09b978e3df9a722aaeff6d7af1f3eb90ca
kerosene2562 <ipzk241_roa@student.ztu.edu.ua>
2025-05-11 17:55:52 +0300

    iterator

---
+++ Composite/Iterator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3.Composite
+{
+    public abstract class Iterator : IEnumerator
+    {
+        object IEnumerator.Current => Current();
+        public abstract LightNode Current();
+        public abstract int Key();
+        public abstract bool MoveNext();
+        public abstract void Reset();
+    }
+}
